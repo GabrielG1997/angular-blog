ef8a0da912d32b65e45ab5da4383bba6646e3a3a
Gabriel Guimarães <gabriel.alvesd16@gmail.com>
2023-10-14 10:05:43 -0300

    Project changes, routings added, new pages and components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "O Disney+ anunciou uma nova série altamente antecipada, intitulada \"Starship Chronicles\", uma aventura épica de ficção científica que levará os espectadores a uma jornada intergaláctica emocionante. A série promete explorar um vasto universo repleto de planetas alienígenas, tecnologia avançada e encontros com raças extraterrestres intrigantes. Com uma narrativa envolvente, personagens cativantes e efeitos visuais deslumbrantes.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "Um novo filme do Pantera Negra está prestes a ser lançado, trazendo de volta o lendário herói de Wakanda para mais uma emocionante aventura no Universo Cinematográfico da Marvel.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpghttps://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "O diretor confirmou que o aguardado novo filme dos X-Men está atualmente em pós-produção, gerando grande expectativa entre os fãs para a próxima aventura mutante nas telonas.",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  }
+]
